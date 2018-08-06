8e63f0dce7e3b9eb365847da060c452c1a7b5b95
fazley <fazleysanusi8@gmail.com>
2018-08-07 00:00:08 +0800

    add arithmetic algo

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    /* Author: Fazley Sanusi
+        Date : 2018-08-06
+        Desc : display "Hello Jave " on the screen
+    * */
+    public static void main(String[] args)
+    {
+        System.out.print("Hello Java");
+    }
+}
