ada4fd2c4bff7b83a3b0ba0df28646f6657333ca
fazley <fazleysanusi8@gmail.com>
2018-08-07 22:50:06 +0800

    add Assignment Operation

---
+++ src/ChainAssign.java
@@ -0,0 +1,10 @@
+public class ChainAssign {
+    public static void main(String[] args){
+        int a, b, c;
+
+        a = b = c = 100; //set a b c to 100
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
