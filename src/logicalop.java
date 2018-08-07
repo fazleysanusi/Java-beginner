05cd9e73c578cea1fa90acc225f65a451a0b2de2
fazley <fazleysanusi8@gmail.com>
2018-08-07 08:15:12 +0800

    add relational operation

---
+++ src/logicalop.java
@@ -0,0 +1,15 @@
+public class logicalop{
+    public static void main(String[] args){
+        //Variables Definition and Initialization
+        boolean bool1 = true, bool2 = false;
+
+        //Logical AND
+        System.out.println("bool1 && bool2 = " + (bool1 && bool2));
+
+        //Logical OR
+        System.out.println("bool1 || bool2 = " + (bool1 || bool2));
+
+        // Logical NOT
+        System.out.println("!(bool1 && bool2) = " + !(bool1 && bool2));
+    }
+}
